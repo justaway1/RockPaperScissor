c25b2d16751305784015b4a6883fb85fa28e442f
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-08-27 02:00:42 +0300

    added little logic!

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,7 @@
 //Const variables
 
-const result = document.getElementById('results');
+const result = document.getElementById('result-win');
+const resultWithWhat = document.getElementById('result-withWhat')
 const humanWeapon = document.getElementById('human-weapon');
 const cpuWeapon = document.getElementById('cpu-weapon');
 const humanScore = document.getElementById('human-score');
@@ -17,28 +18,52 @@ rock.addEventListener('click', handleRock);
 paper.addEventListener('click', handlePaper);
 scissor.addEventListener('click', handleScissor);
 
+//CPU RNG Generator from 1 to 3
 function computerChoice() {
-    return Math.floor(Math.random() * 3)
+    return Math.floor(1 + Math.random() * 3)
 }
 
+//ROCK EVENT HANDLER
 function handleRock(e, cpuChoice) {
     cpuChoice = computerChoice();
-    if (e.target === rock) {
-        humanWeapon.innerHTML = "<img src='./images/rock.png'>"
-        humanWeapon.classList.add('selectedImage');
-    }
-    if (cpuChoice === 1) {
-        cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
-        cpuWeapon.classList.add('selectedImage');
-    } else if (cpuChoice === 2) {
-        cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
-        cpuWeapon.classList.add('selectedImage');
-    } else if (cpuChoice === 3) {
-        cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
-        cpuWeapon.classList.add('selectedImage');
+
+    let humanScores = 0;
+    let cpuScores = 0;
+
+    for (let i = 0; i <= 5; i++) {
+
+        if (e.target === rock) {
+            humanWeapon.innerHTML = "<img src='./images/rock.png'>"
+            humanWeapon.classList.add('selectedImage');
+
+        }
+        if (cpuChoice === 1) {
+            cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
+            cpuWeapon.classList.add('selectedImage');
+            result.innerHTML = `It's a Tie`
+            resultWithWhat.innerHTML = `Rock with Rock`
+
+        } else if (cpuChoice === 2) {
+            cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
+            cpuWeapon.classList.add('selectedImage');
+            result.innerHTML = `You Lost!`
+            resultWithWhat.innerHTML = `Paper beats Rock!`
+            cpuScores++
+            cpuScore.innerHTML = `Computer Score: ${cpuScores}`;
+
+        } else if (cpuChoice === 3) {
+            cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
+            cpuWeapon.classList.add('selectedImage');
+            result.innerHTML = `You Won!`
+            resultWithWhat.innerHTML = `Rock beats Scissors!`
+            humanScores++;
+            humanScore.innerHTML = `Human Score: ${humanScores}`;
+
+        }
     }
 }
 
+//PAPER EVENT HANDLER
 function handlePaper(e, cpuChoice) {
     cpuChoice = computerChoice();
     if (e.target === paper) {
@@ -55,6 +80,8 @@ function handlePaper(e, cpuChoice) {
         cpuWeapon.classList.add('selectedImage');
     }
 }
+
+//SCISSOR EVENT HANDLER
 function handleScissor(e) {
     cpuChoice = computerChoice();
     if (e.target === scissor) {
@@ -71,6 +98,5 @@ function handleScissor(e) {
         cpuWeapon.classList.add('selectedImage');
     }
 }
-
 
 
