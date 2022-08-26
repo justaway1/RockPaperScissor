7133f58a36d1d1cd8a7a52e19f621c9ee9d96382
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-08-27 02:49:50 +0300

    Got the points to work as intended!

--- mainscript.js
+++ mainscript.js
@@ -23,79 +23,98 @@ function computerChoice() {
     return Math.floor(1 + Math.random() * 3)
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 //ROCK EVENT HANDLER
 function handleRock(e, cpuChoice) {
     cpuChoice = computerChoice();
 
-    let humanScores = 0;
-    let cpuScores = 0;
+    if (e.target === rock) {
+        humanWeapon.innerHTML = "<img src='./images/rock.png'>"
+        humanWeapon.classList.add('selectedImage');
 
-    for (let i = 0; i <= 5; i++) {
-
-        if (e.target === rock) {
-            humanWeapon.innerHTML = "<img src='./images/rock.png'>"
-            humanWeapon.classList.add('selectedImage');
-
-        }
-        if (cpuChoice === 1) {
-            cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
-            cpuWeapon.classList.add('selectedImage');
-            result.innerHTML = `It's a Tie`
-            resultWithWhat.innerHTML = `Rock with Rock`
-
-        } else if (cpuChoice === 2) {
-            cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
-            cpuWeapon.classList.add('selectedImage');
-            result.innerHTML = `You Lost!`
-            resultWithWhat.innerHTML = `Paper beats Rock!`
-            cpuScores++
-            cpuScore.innerHTML = `Computer Score: ${cpuScores}`;
-
-        } else if (cpuChoice === 3) {
-            cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
-            cpuWeapon.classList.add('selectedImage');
-            result.innerHTML = `You Won!`
-            resultWithWhat.innerHTML = `Rock beats Scissors!`
-            humanScores++;
-            humanScore.innerHTML = `Human Score: ${humanScores}`;
-
-        }
     }
+    if (cpuChoice === 1) {
+        cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
+        cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `It's a Tie`
+        resultWithWhat.innerHTML = `Rock with Rock`
+
+    } if (cpuChoice === 2) {
+        cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
+        cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Lost!`
+        resultWithWhat.innerHTML = `Paper beats Rock!`
+        computerScore++;
+        cpuScore.innerHTML = `Computer Score: ${computerScore}`;
+
+    } if (cpuChoice === 3) {
+        cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
+        cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Won!`
+        playerScore++;
+        resultWithWhat.innerHTML = `Rock beats Scissors!`
+        humanScore.innerHTML = `Human Score: ${playerScore}`;
+    }
+
 }
 
 //PAPER EVENT HANDLER
 function handlePaper(e, cpuChoice) {
     cpuChoice = computerChoice();
+
     if (e.target === paper) {
         humanWeapon.innerHTML = "<img src='./images/paper.png'>"
         humanWeapon.classList.add('selectedImage')
+
     } if (cpuChoice === 1) {
         cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Won!`
+        playerScore++;
+        resultWithWhat.innerHTML = `Paper beats Rock!`
+        humanScore.innerHTML = `Human Score ${playerScore}`
+
     } else if (cpuChoice === 2) {
         cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `It's a Tie!`
+        resultWithWhat.innerHTML = `Paper with Paper!`
+
     } else if (cpuChoice === 3) {
         cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Lost!`
+        resultWithWhat.innerHTML = `Scissors beats Paper!`
     }
 }
 
 //SCISSOR EVENT HANDLER
 function handleScissor(e) {
     cpuChoice = computerChoice();
+
     if (e.target === scissor) {
         humanWeapon.innerHTML = "<img src='./images/scissor.png'>"
         humanWeapon.classList.add('selectedImage')
+
     } if (cpuChoice === 1) {
         cpuWeapon.innerHTML = "<img src='./images/rock.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Lost!`
+        resultWithWhat.innerHTML = `Rock beats Scissors!`
+
     } else if (cpuChoice === 2) {
         cpuWeapon.innerHTML = "<img src='./images/paper.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `You Won!`
+        resultWithWhat.innerHTML = `Scissors beats Paper!`
+
     } else if (cpuChoice === 3) {
         cpuWeapon.innerHTML = "<img src='./images/scissor.png'>"
         cpuWeapon.classList.add('selectedImage');
+        result.innerHTML = `It's a Tie!`
+        resultWithWhat.innerHTML = `Scissors with Scissors!`
     }
 }
 
